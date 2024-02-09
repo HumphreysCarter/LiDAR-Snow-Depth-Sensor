61c6c0a041230a84a1e0cd334e86b3e87df788be
Carter Humphreys <carter.humphreys@lake-effect.dev>
2024-02-08 18:46:28 -0700

    Added units support to WeeWx StdService

--- src/weewx/lidar_snow.py
+++ src/weewx/lidar_snow.py
@@ -1,7 +1,9 @@
 import json
 import weewx
+import weewx.units
 from weewx.engine import StdService
 from datetime import datetime
+
 
 class AddLidarSnowData(StdService):
 
@@ -13,10 +15,23 @@ class AddLidarSnowData(StdService):
         # Bind to any new archive record events:
         self.bind(weewx.NEW_ARCHIVE_RECORD, self.new_archive_record)
 
+        # Get unit system in use
+        unit_system = config_dict['StdConvert']['target_unit']  # Options are 'US', 'METRICWX', or 'METRIC'
+        if unit_system == 'US':
+            self.units_snow = weewx.units.USUnits['group_rain']
+            self.units_temp = weewx.units.USUnits['group_temperature']
+        elif unit_system == 'METRIC':
+            self.units_snow = weewx.units.MetricUnits['group_rain']
+            self.units_temp = weewx.units.MetricUnits['group_temperature']
+        elif unit_system == 'METRICWX':
+            self.units_snow = weewx.units.MetricWXUnits['group_rain']
+            self.units_temp = weewx.units.MetricWXUnits['group_temperature']
+
         # Get data from config
         self.api_directory = '/home/pi/lidar-snow/data/api/'
         self.stale_minutes = 2
         self.sensor_id = 'b0b21c043834'
+        self.sensor_datum = 103
 
     def get_sensor_data(self, serial_number):
         with open(f'{self.api_directory}/LiDAR_SnowDepth_{serial_number}.json', 'r') as f:
@@ -24,7 +39,7 @@ class AddLidarSnowData(StdService):
 
         # Only return data if data no more than 1 minute old
         data_date = datetime.strptime(data['datetime'], '%Y-%m-%dT%H:%M:%S.%f')
-        if (datetime.utcnow()-data_date).total_seconds() <= self.stale_minutes * 60:
+        if (datetime.utcnow() - data_date).total_seconds() <= self.stale_minutes * 60:
             return data
 
         return None
@@ -41,6 +56,39 @@ class AddLidarSnowData(StdService):
             data = self.get_sensor_data(self.sensor_id)
 
             if data is not None:
-                event.record['lidar_distance'] = self.get_value(data, 'average_distance')
-                event.record['lidar_boardtemp'] = self.get_value(data, 'board_temp')
+                # Get distance from LiDAR sensor
+                lidar_distance = self.get_value(data, 'average_distance')
+
+                if lidar_distance is not None:
+                    # Calculate snow depth from distance and datum
+                    lidar_snowdepth = self.sensor_datum-lidar_distance
+
+                    # Convert units as needed, sensor data is in cm.
+                    if self.units_snow != 'cm':
+                        conversion = weewx.units.conversionDict['cm'][self.units_snow]
+                        lidar_distance = conversion(lidar_distance)
+                        lidar_snowdepth = conversion(lidar_snowdepth)
+
+                    # Add data to record
+                    event.record['lidar_distance'] = lidar_distance
+                    event.record['lidar_snowdepth'] = lidar_snowdepth
+
+                else:
+                    event.record['lidar_distance'] = None
+                    event.record['lidar_snowdepth'] = None
+
+                # Add sensor board temperature
+                lidar_boardtemp = self.get_value(data, 'board_temp')
+
+                if lidar_boardtemp is not None:
+                    # Convert from degC if needed
+                    if self.units_temp != 'degree_C':
+                        conversion = weewx.units.conversionDict['degree_C'][self.units_temp]
+                        lidar_boardtemp = conversion(lidar_boardtemp)
+
+                    event.record['lidar_boardtemp'] = lidar_boardtemp
+                else:
+                    event.record['lidar_boardtemp'] = None
+
+                # Add sensor WiFi signal
                 event.record['lidar_signal'] = self.get_value(data, 'wifi')
